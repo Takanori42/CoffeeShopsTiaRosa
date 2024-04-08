495a4c6d893014e807042b21e8fe04d9827e56ba
Gabriela <gaby.nagato123@gmail.com>
2024-04-08 18:04:50 -0300

    Ajustando resposividade

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/landingpage/header';
 import Items from './components/landingpage/items';
+import Welcome from './components/landingpage/welcome';
 
 function App() {
   return (
     <main>
       <Header></Header>
       <Items></Items>
+      <Welcome></Welcome>
     </main>
   );
 }
---
+++ src/components/landingpage/welcome/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./style.css";
+
+export default function Welcome() {
+    return (
+        <section className="presentation">
+            <div className="welcome">
+                <div className="welcome-container">
+                    <div className="welcome-title">
+                        <h1>Bem-vindo!</h1>
+                    </div>
+                    <hr className="division"></hr>
+                    <div className="welcome-text">
+                        <p>Em nossa cafeteria, cada gole de café é uma
+                            viagem de sabor. Com um ambiente acolhedor e baristas
+                            especializados, oferecemos uma variedade de bebidas e
+                            delícias caseiras. Venha nos visitar para
+                            uma experiência única de café.</p>
+                    </div>
+                </div>
+                <div>
+                    <img className="img" src="mulher.png"></img>
+                </div>
+            </div>
+        </section>
+    )
+}
